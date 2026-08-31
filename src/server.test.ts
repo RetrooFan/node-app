c66842156a34bb17ec2ef3e72210199f541120b2
Krzysztof Sobol <krzysieksobol93@gmail.com>
2026-08-31 20:06:34 +0200

    add tests

---
+++ src/server.test.ts
@@ -0,0 +1,12 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import request from 'supertest';
+
+import { app } from './server.js';
+
+test('GET / returns Hello, World!', async () => {
+  const response = await request(app).get('/');
+
+  assert.equal(response.status, 200);
+  assert.equal(response.text, 'Hello, World!');
+});

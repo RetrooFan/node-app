0dae65477966aa851d35811efb8c2868b8cfb532
Krzysztof Sobol <krzysieksobol93@gmail.com>
2026-08-31 20:55:11 +0200

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+];
